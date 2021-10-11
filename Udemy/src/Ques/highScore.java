b7d06b4bb7044595e15de21ef6e3ac15bb5a5630
VarzikaT <varzikatiwari08@gmail.com>
2021-10-12 01:06:48 +0530

    created.

--- Udemy/src/Ques/highScore.java
+++ Udemy/src/Ques/highScore.java
@@ -4,7 +4,13 @@ public class highScore {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String j = "Pankaj";
+		int p = 2;
+		highScore1(j,p);
+	}
+		
+		public static void highScore1(String s, int pos) {
+		System.out.println( s + " managed to get a position of " + pos  + " on the high score table." );
 	}
 
 }
