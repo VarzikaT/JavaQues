08a138acc4f1ad445f6b66384f4a02407dd49c86
VarzikaT <varzikatiwari08@gmail.com>
2021-10-18 15:32:16 +0530

    swap 1 method added

--- Competetive Classes/src/Questions/array.java
+++ Competetive Classes/src/Questions/array.java
@@ -14,9 +14,17 @@ public class array {
 //		System.out.println(arr[0]+" "+arr[1]);
 		int other[]= {100,200,300};
 		System.out.println(arr[0]+" "+other[0]);
-		swap(arr, other);
-		System.out.println(arr[0]+" "+other[0]);
+		//swap(arr, other);
+		swap1(arr,0,1);
+		System.out.println(arr[0]+" "+arr[1]);
 		
+	}
+	//java is call by reference therefore unless we change the values at the reference or original address they are not reflected
+	//since in  the swap method created before we were creating copies and changing there
+	public static void swap1(int []a, int i , int j) {
+		int t = a[i];
+		a[i]=a[j];
+		a[j]=t;
 	}
 	public static void swap(int []a, int []b) {
 		int[] t=a;
