4ddab4e4f137c8b4bda6e499f9d2e371057f0160
VarzikaT <varzikatiwari08@gmail.com>
2021-10-18 14:53:27 +0530

    array declaration

--- Competetive Classes/src/Questions/array.java
+++ Competetive Classes/src/Questions/array.java
@@ -1,5 +1,19 @@
 package Questions;
 
 public class array {
-
+	public static void main(String[] args) {
+		//int [] arr=null;
+//		System.out.println(arr);
+//		arr = new int[5];
+//		//length
+//		System.out.println(arr.length);
+//		
+		int arr[] = {10,20,30,40,50};
+		System.out.println(arr[0]+" "+arr[1]);
+		swap(arr[0], arr[1]);
+		System.out.println(arr[0]+" "+arr[1]);
+	}
+	public static void swap(int a, int b) {
+		
+	}
 }
