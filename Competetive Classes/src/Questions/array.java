e62fb4b6a3a75d402db7dbe0ea1523ded31c1718
VarzikaT <varzikatiwari08@gmail.com>
2021-10-18 15:08:46 +0530

    swap method added

--- Competetive Classes/src/Questions/array.java
+++ Competetive Classes/src/Questions/array.java
@@ -9,11 +9,18 @@ public class array {
 //		System.out.println(arr.length);
 //		
 		int arr[] = {10,20,30,40,50};
-		System.out.println(arr[0]+" "+arr[1]);
-		swap(arr[0], arr[1]);
-		System.out.println(arr[0]+" "+arr[1]);
-	}
-	public static void swap(int a, int b) {
+//		System.out.println(arr[0]+" "+arr[1]);
+//		swap(arr[0], arr[1]);
+//		System.out.println(arr[0]+" "+arr[1]);
+		int other[]= {100,200,300};
+		System.out.println(arr[0]+" "+other[0]);
+		swap(arr, other);
+		System.out.println(arr[0]+" "+other[0]);
 		
+	}
+	public static void swap(int []a, int []b) {
+		int[] t=a;
+		a=b;
+		b=t;
 	}
 }
