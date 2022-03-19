f4c6ace61ffd062c674e5e7fad5dc04158ccb7fc
VarzikaT <varzikatiwari08@gmail.com>
2022-03-19 19:20:22 +0530

    .

--- BasicQues/src/ques/Factorial.java
+++ BasicQues/src/ques/Factorial.java
@@ -1,13 +1,13 @@
 package ques;
 
 public class Factorial {
-	public static void msin(String args[]) {
+	public static void main(String args[]) {
 		int i,fact=1;
 		int number=6;
 		for(i=1;i<number;i++) {
 			fact=fact*i;
 		}
-		System.out.println("Factorial of"+ number+"is"+fact);
+		System.out.println("Factorial of "+ number+" is "+fact);
 	}
 }
 
