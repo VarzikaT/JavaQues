6541d6dabf14f61f534665e852cd19033b111604
VarzikaT <varzikatiwari08@gmail.com>
2022-03-19 19:05:01 +0530

    program.

--- BasicQues/src/ques/Currentdate_time.java
+++ BasicQues/src/ques/Currentdate_time.java
@@ -1,5 +1,8 @@
 package ques;
-
+import java.time.LocalDateTime;
 public class Currentdate_time {
-
+	public static void main(String args[]) {
+		LocalDateTime current= LocalDateTime.now();
+		System.out.println("Current Date and time:"+current);
+	}
 }
