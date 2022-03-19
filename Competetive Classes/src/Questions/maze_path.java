0fa677912592a7dda9d8492b7184b6ed50491030
VarzikaT <varzikatiwari08@gmail.com>
2022-03-19 18:56:45 +0530

    simple fibonacci program

---
+++ Competetive Classes/src/Questions/maze_path.java
@@ -0,0 +1,12 @@
+package Questions;
+
+public class maze_path {
+	public static void main(String args[]) {
+		int row = 3;
+		int col = 4;
+		mazepath(0,0,3,4,"");
+	}
+	public static void mazepath(int cr, int cc, int er, int ec, String ans) {
+		
+	}
+}
